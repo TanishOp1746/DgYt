7402539075a08eb51100cb13e9b791be46dff2e1
tanishgaming174 <26605407-tanishgaming174@users.noreply.replit.com>
2024-01-18 08:42:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const server = express();
+const mineflayer = require('mineflayer');
+
+const mySecret = process.env['Password']
+
+server.all(`/`, (req, res) => {
+    res.send(`Please connect me to a hosting website in-order to work 24/7.`);
+});
+
+function keepAlive() {
+    server.listen(3000, () => {
+        console.log(`24/7 Activation Complete`);
+    });
+}
+
+function createBot() {
+    const bot = mineflayer.createBot({
+      host: 'Play.potionmc.xyz',
+      username: 'RedstoneSorcer33',
+      password: 'haseeb',
+    });
+
+    bot.on('login', () => {
+      console.log('Bot connected to server');
+    });
+
+    bot.on('spawn', () => {
+      bot.chat(process.env["Password"]);
+   setTimeout(() => {
+     bot.chat('');
+   }, 10000 );
+    });
+
+    bot.on('chat', (username, message) => {
+      if (username !== bot.username) {
+        bot.chat(``);
+        console.log(`${username} said "${message}"`);
+      }
+    });
+  
+    bot.on('end', () => {
+      console.log('Bot disconnected');
+      createBot(); // reconnects when bot gets disconnected
+    });
+
+    bot.on('error', (error) => {
+      console.log('An error has occurred:', error);
+      if (error.message === 'Client Timeout After 30000 milliseconds') {
+        console.log('Reconnecting...');
+        createBot(); // reconnects when a timeout error occurs
+        }
+    });
+    }
+           
+createBot(); // initial bot creation
+keepAlive();

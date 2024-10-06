5fe603f65796cd06fabb082c6161f95dec4b74f7
TanishOp1746 <TanishPunjabi5@gmail.com>
2024-10-06 11:39:21 +0530

    Index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = express();
 const mineflayer = require('mineflayer');
 
 server.all(`/`, (req, res) => {
-    res.send(`Please connect me to a hosting website in order to work 24/7.`);
+    res.send(`Please connect me to a hosting website in-order to work 24/7.`);
 });
 
 function keepAlive() {
@@ -11,53 +11,47 @@ function keepAlive() {
         console.log(`24/7 Activation Complete`);
     });
 }
-let isConnected = false;
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'Potionmc.xyz',
-        username: 'DgYtOnTop',
-        password: 'haseeb',
+      host: 'Play.potionmc.xyz',
+      username: 'DgytonTop',
+      password: 'haseeb',
     });
 
     bot.on('login', () => {
-        console.log('Bot connected to server');
+      console.log('Bot connected to server');
     });
 
     bot.on('spawn', () => {
-        bot.chat('/login 553532');
-        setTimeout(() => {
-            bot.chat('/server survival');
-        }, 10000);
+      bot.chat('/login 553532');
+   setTimeout(() => {
+     bot.chat('/server survival');
+   }, 10000 );
     });
-
-    setInterval(() => {
+setInterval(() => {
         bot.chat('/server Survival');
-    }, 300000); // 300000 milliseconds = 5 minutes
-
-     // Check the bot's connection status every 30 minutes
-    setInterval(() => {
-        if (isConnected) {
-            console.log('Bot is connected.');
-        } else {
-            console.log('Bot is not connected.');
-        }
-    }, 30 * 60 * 1000); // 30 minutes in milliseconds
-
-
+    }, 120000); 
+    bot.on('chat', (username, message) => {
+      if (username !== bot.username) {
+        bot.chat(``);
+        console.log(`${username} said "${message}"`);
+      }
+    });
+  
     bot.on('end', () => {
-        console.log('Bot disconnected');
-        createBot(); // reconnects when bot gets disconnected
+      console.log('Bot disconnected');
+      createBot(); // reconnects when bot gets disconnected
     });
 
     bot.on('error', (error) => {
-        console.log('An error has occurred:', error);
-        if (error.message === 'Client Timeout After 30000 milliseconds') {
-            console.log('Reconnecting...');
-            createBot(); // reconnects when a timeout error occurs
+      console.log('An error has occurred:', error);
+      if (error.message === 'Client Timeout After 30000 milliseconds') {
+        console.log('Reconnecting...');
+        createBot(); // reconnects when a timeout error occurs
         }
     });
-}
-
+    }
+           
 createBot(); // initial bot creation
 keepAlive();
